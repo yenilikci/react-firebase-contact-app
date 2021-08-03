c7bcf46c2cfe9ce9be5eb68dfd7e114d493801ed
yenilikci <m.melihcelik017@outlook.com>
2021-08-03 17:17:23 +0300

    Add fields to the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 export default function ContactForm() {
     return (
-        <div>
-            iletişim formu
-        </div>
+        <form>
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="İsim giriniz" />
+            </div>
+            <div className="form-group input-group">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">
+                        <i className="fas fa-user"></i>
+                    </div>
+                </div>
+                <input className="form-control" placeholder="Soyisim giriniz" />
+            </div>
+            <div className="form-row">
+                <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-phone"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Telefon giriniz" />
+                </div>
+                <div className="form-group input-group col-md-6">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">
+                            <i className="fas fa-envelope-square"></i>
+                        </div>
+                    </div>
+                    <input className="form-control" placeholder="Mail adresi giriniz" />
+                </div>
+            </div>
+            <div className="form-group">
+                <input type="submit" value="Kaydet" className="btn btn-success btn-block" />
+            </div>
+        </form>
     )
 }
