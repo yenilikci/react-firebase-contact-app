627ffe7a7c732e6549c5edc74704313c6dfea22b
yenilikci <m.melihcelik017@outlook.com>
2021-08-03 21:12:06 +0300

    Add contact info list

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
     useEffect(() => {
         //get data
         firebaseDB.child('iletisim').on('value', snapshot => {
-            if(snapshot.val() != null){
+            if (snapshot.val() != null) {
                 setIletisimVeriler({
                     ...snapshot.val()
                 })
@@ -18,6 +18,7 @@ export default function Contact() {
         })
     }, [])
 
+    //push data
     const veriEkle = nesne => {
         //console.log(nesne);
         firebaseDB.child('iletisim').push(
@@ -44,7 +45,35 @@ export default function Contact() {
                     <ContactForm ekle={veriEkle} />
                 </div>
                 <div className="col-md-7">
-                    İletişim bilgileri listesi
+                    <table className="table table-borderless table-strip">
+                        <thead className="thead-light">
+                            <tr>
+                                <th>İsim-Soyisim</th>
+                                <th>Mail</th>
+                                <th>Telefon</th>
+                                <th>İşlem</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                Object.keys(iletisimVeriler).map(id => {
+                                    return <tr key={id}>
+                                        <td>{iletisimVeriler[id].isim} {iletisimVeriler[id].soyisim}</td>
+                                        <td>{iletisimVeriler[id].mail}</td>
+                                        <td>{iletisimVeriler[id].telefon}</td>
+                                        <td>
+                                            <a className="btn text-primary">
+                                                <i className="fas fa-pencil-alt"></i>
+                                            </a>
+                                            <a className="btn text-primary">
+                                                <i className="fas fa-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
