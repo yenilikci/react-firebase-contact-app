bec599b29dd83971f5b6ec242003cdd4a461c2ec
yenilikci <m.melihcelik017@outlook.com>
2021-08-03 16:53:37 +0300

    Firebase sdk included in project

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBDUWVoVrjIazXndcQ2kIabBzwP0pVy1TU",
+    authDomain: "react-contact-realtime.firebaseapp.com",
+    databaseURL: "https://react-contact-realtime-default-rtdb.firebaseio.com",
+    projectId: "react-contact-realtime",
+    storageBucket: "react-contact-realtime.appspot.com",
+    messagingSenderId: "678844425486",
+    appId: "1:678844425486:web:8286db3b8f201493aca35f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
